c79fa47417aa239c2884ecfecd37c6847a70cfb1
JenkinChen <121633996@qq.com>
2015-10-20 22:02:38 +0800

    第一个github仓库

---
+++ PRJ_GeneralFramework_ObjC/ViewControllers/Base/BaseViewCtrl.h
@@ -0,0 +1,13 @@
+//
+//  BaseViewCtrl.h
+//  PRJ_GeneralFramework_ObjC
+//
+//  Created by C on 15/10/20.
+//  Copyright © 2015年 C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseViewCtrl : UIViewController
+
+@end
